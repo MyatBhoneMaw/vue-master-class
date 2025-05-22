872d4e0b0849ceec0da259b2195780ecf3375ab2
Bue <myatbhonemaw72@gmail.com>
2025-05-22 19:51:26 +0630

    Refactor Vue.js Codebase for File-Based Routing

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <RouterLink :to="{name : '/projects/[id]', params : { id : 1} }">Go To Project 1</RouterLink>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
+
+
+
+<!-- vue route link for use single application 
+with a tag run to link page refresh 
+but RouteLin not refresh and support for single Page -->
+
+
+ <!-- Enhanced Routing with unPlugin vue router 
+ from vue school -->
