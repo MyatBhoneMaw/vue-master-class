f71590e2a3ab9f08a27b8ebb78cf0af91de4fa57
Bue <myatbhonemaw72@gmail.com>
2025-05-25 01:51:15 +0630

    seeding-the-supabase-remote-database-with-fakerjs

--- database/seed.js
+++ database/seed.js
@@ -2,15 +2,18 @@
 
 import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
-// In case you're using Node version older than v20
-// Install dotenvx by running the following command npm install @dotenvx/dotenvx --save
-// Next, add the following code to the file
-//
-// import { config } from '@dotenvx/dotenvx'
-// config()
-//
-// With that done, you can now use env variables as usual
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-console.log(supabase)
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+
+  await supabase.from('projects').insert({
+    name: name,
+    slug: name.toLocaleLowerCase().replace(/ /g, '-'),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElements([1, 2, 3])
+  })
+}
+
+await seedProjects()
