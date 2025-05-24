18ea54f7d167d9e4d6214e5c8a24e1df63569219
Bue <myatbhonemaw72@gmail.com>
2025-05-24 22:34:08 +0630

    set-up-supabase-javascript-client-in-node-environment

--- database/seed.js
+++ database/seed.js
@@ -1,10 +1,16 @@
-import { fakerEN_US as faker } from "@faker-js/faker";
-//fakerEN_US is language 
-//installed jaker.js
+/* eslint-env node */
 
+import { fakerEN_US as faker } from '@faker-js/faker'
+import { createClient } from '@supabase/supabase-js'
+// In case you're using Node version older than v20
+// Install dotenvx by running the following command npm install @dotenvx/dotenvx --save
+// Next, add the following code to the file
+//
+// import { config } from '@dotenvx/dotenvx'
+// config()
+//
+// With that done, you can now use env variables as usual
 
+const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-const personName = faker.person.fullName();
-const personBio = faker.person.bio();
-
-console.log("hi My name is" , personName , '. My bio is' , personBio);
+console.log(supabase)
