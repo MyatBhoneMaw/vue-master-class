8973f26c9f95381b706f3870d243144b4ffaed9b
Bue <myatbhonemaw72@gmail.com>
2025-05-25 23:46:34 +0630

    introduction-to-vue-js-reactivity-system-and-using-refs

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,11 +2,23 @@
     <div>
         <h1>Home Projects</h1>
         <RouterLink to="/">Go Home</RouterLink>
+        <div>{{ projects }}</div>
     </div>
 </template>
 
 <script setup>
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+ const projects = ref()
 
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+  projects.value = data
+
+    console.log("projects", data);
+})()
 </script>
 
 <style lang="css" scoped>
