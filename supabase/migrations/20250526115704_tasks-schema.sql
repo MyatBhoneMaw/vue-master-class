e3a27430d7b9c43bc5abcc6eaff4bd1a1b30b881
Bue <myatbhonemaw72@gmail.com>
2025-05-26 18:55:00 +0630

    create-a-new-page-for-tasks-with-database-migration-seed-and-types

---
+++ supabase/migrations/20250526115704_tasks-schema.sql
@@ -0,0 +1,17 @@
+
+drop table if exists tasks;
+
+-- drop type if exists current_status;
+-- create type current_status as enum ('in-progress', 'completed');
+
+create table 
+  tasks (
+    id bigint primary key generated always as identity not null,
+    created_at timestamptz default now() not null,
+    name text not null,
+    status current_status default 'in-progress' not null,
+    description text not null,
+    due_date date default null,
+    project_id bigint references projects (id) default null,
+    collaborators text array default array[]::varchar[] not null
+  );
