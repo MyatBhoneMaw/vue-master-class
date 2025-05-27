957843a0a51b6d0db96de8cafdbce0fc2e8ca96d
Bue <myatbhonemaw72@gmail.com>
2025-05-27 19:05:13 +0630

    use-lucide-icons-with-iconify-and-vue-js

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,50 @@
 <script setup lang="ts">
-import Input from '@/components/ui/input/Input.vue'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+    import Input from '@/components/ui/input/Input.vue'
+    import {
+        DropdownMenu,
+        DropdownMenuContent,
+        DropdownMenuItem,
+        DropdownMenuLabel,
+        DropdownMenuSeparator,
+        DropdownMenuTrigger
+    } from '@/components/ui/dropdown-menu'
+    import {
+        Avatar,
+        AvatarFallback,
+        AvatarImage
+    } from '@/components/ui/avatar'
 </script>
 
 <template>
-  <nav class="h-16 border-b border-gray-900 bg-muted/40 flex justify-between px-6 items-center">
-    <form class="w-full max-w-96">
-      <Input class="w-full pl-8 bg-gray-500" type="text" placeholder="Search ..." />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <hr class="bg-gray-500">
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+    <nav class="h-16 border-b border-gray-900 bg-muted/40 flex justify-between px-6 items-center">
+        <form class="w-full max-w-96">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <path d="m21 21l-4.34-4.34" />
+                    <circle cx="11" cy="11" r="8" />
+                </g>
+            </svg>
+            <Input class="w-full pl-8 bg-gray-500" type="text" placeholder="Search ..." />
+        </form>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <hr class="bg-gray-500">
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </nav>
+    <main>
+        <RouterView />
+    </main>
 </template>
