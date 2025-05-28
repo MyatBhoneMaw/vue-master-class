87988c7cf248750d938aef9a2a7456ac2646355a
Bue <myatbhonemaw72@gmail.com>
2025-05-28 22:34:18 +0630

    integrate-unplugin-auto-import-with-vue-and-vite

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+// import { useRoute } from 'vue-router';
 
 const route = useRoute();
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,37 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// vite.config.ts
+import AutoImport from 'unplugin-auto-import/vite'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue({
+  plugins: [VueRouter(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        'vue-router'
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+       }),
+    vue({
     template : {
       compilerOptions : {
         isCustomElement : (element) => element.startsWith('iconify-icon')
       }
-    }
+    },
+    
   })],
   resolve: {
     alias: {
