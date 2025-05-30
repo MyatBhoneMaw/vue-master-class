a0222c640e3fbc1176d3868ce71db3ed9b967fd8
Bue <myatbhonemaw72@gmail.com>
2025-05-30 18:31:40 +0630

    Use Vue.js Suspense Component to Handle Async Dependencies

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,33 +1,52 @@
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-// import { h, ref } from 'vue'
 import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 
 const projects = ref<Tables<'projects'>[] | null>(null)
-;(async () => {
+const getProjects = async () => {
   const { data, error } = await supabase.from('projects').select()
 
   if (error) console.log(error)
 
   projects.value = data
+}
 
-  console.log('projects: ', projects.value)
-})()
+await getProjects()
 
 const columns: ColumnDef<Tables<'projects'>>[] = [
   {
     accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name')
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.slug}`,
+          class: 'text-left font-medium hover:bg-muted block w-full'
+        },
+        () => row.getValue('name')
+      )
+    }
   },
   {
     accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status')
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    }
   },
   {
     accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators')
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h(
+        'div',
+        { class: 'text-left font-medium' },
+        JSON.stringify(row.getValue('collaborators'))
+      )
+    }
   }
 ]
 </script>
